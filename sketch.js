b89820ee8e833a850df659e4691dc0726212bb44
elkolorado <tygryski178@gmail.com>
2018-09-16 18:13:41 +0200

    Added more links, and a html table

--- sketch.js
+++ sketch.js
@@ -1,3 +1,48 @@
+var textfield;
+var npc;
+var output;
+var input;
+var submit;
+
+function preload(){
+  npc = loadStrings("npc.txt");
+}
+
+function setup() {
+  noCanvas();
+  textfield = select("#txt");
+  output = select('#output');
+  submit = select('#submit');
+  submit.mousePressed(newText);
+  noLoop();
+  // npc.indexOf(textfield.value()) > -1  // found results
+}
+
+
+var arr =[];
+function newText(input) {
+  var b = 94;
+  var str1 = textfield.value();
+  var liczby = createDiv(str1)
+    for (n=0; n<=str1.length-b; n++){
+      res = str1.substring(n, n+b);
+      arr[n]=res;
+      push();
+    }
+  }
+
+var texting = textfield.value();
+var oneline;
+function find(){
+for (var i=0; i < npc.length; i++) {
+  for (var j=0; i < npc.length; j++){
+  npc[i] = oneline[j]
+}
+oneline[j].indexOf("vampire") > -1
+console.log(oneline)
+}
+}
+
 $('a[href*="#"]')
     // Remove links that don't actually link to anything
     .not('[href="#"]')
